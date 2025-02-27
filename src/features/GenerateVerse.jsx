0d7705448ba5c0c1ff870bf7c55a9dcfc4451196
Matt Chastain <work@mattchastain.dev>
2025-02-27 10:41:31 -0500

    add get a random verse functionality, add p element for verse reference

--- src/features/GenerateVerse.jsx
+++ src/features/GenerateVerse.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+import { verses } from '../verses/hopeful_verses.js';
+
 export default function GenerateVerse() {
+	const [verse, setVerse] = useState({});
+	const getRandomVerse = () => {
+		const randomIndex = Math.floor(Math.random() * verses.length);
+		const verse = verses[randomIndex];
+		setVerse({reference: verse.reference, text: verse.text});
+	};
+
 	return (
 		<>
-			<h2 className='my-10'>[Verse Placeholder]</h2>
-			<button className='bg-transparent border-[#f3c199] border-1 rounded-md mt-2'>
+			<p className='mt-10 stroke-current'>{verse.reference}</p>
+			<h2 className='mb-10 max-w-1/2 text-center'>{verse.text}</h2>
+			<button
+				className='bg-transparent border-[#f3c199] border-1 rounded-md mt-2'
+				onClick={getRandomVerse}
+			>
 				Generate Verse
 			</button>
 		</>
